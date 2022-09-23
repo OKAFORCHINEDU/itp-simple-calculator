6a06db361ed641989796d930c35e5e53bd82076f
OKAFORCHINEDU <chneducornelius.okafor@yahoo.com>
2022-09-23 10:58:14 -0700

    Complete function code

--- calculator.py
+++ calculator.py
@@ -3,24 +3,29 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        return "Division Error. Invalid divisor zero(0) not possible"
+
+    else:
+        return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return x * x
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return x ** 0.5
+
